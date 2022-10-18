d8f12f29dda96361618fef8c138132f1e499d8e7
DecSP <nguyencaoson7921@gmail.com>
2022-10-18 19:35:33 +0700

    init

---
+++ setting.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                   X        ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XX X         XX         XX ',
+'                            ',
+'                  XX  XX    ',
+'                  XX  XXX   ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
